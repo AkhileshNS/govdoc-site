04d318cf2877cf5a8086ba6dcb8f0d8cd316e111
AkhileshNS <nsakhilesh02@gmail.com>
2019-12-02 13:21:18 +0530

    removed dummy data

--- src/App/App.store.js
+++ src/App/App.store.js
@@ -14,31 +14,8 @@ class appStore {
   approver = null;
 
   // Common
-  pending = [{
-    name: "Akhilesh Sastry",
-    email: "nsakhilesh@hotmail.com",
-    mobile: "9611129344",
-    approver: {
-      name: "Akhilesh NS",
-      email: "nsakhilesh02@gmail.com"
-    },
-    status: "Pending Approval",
-    url: "https://firebasestorage.googleapis.com/v0/b/shoppinglistplusplus-nsa.appspot.com/o/govdoc%2FLab%20record%20(4).pdf?alt=media&token=7d32e991-dbe8-4dd0-9e2b-61ee659dae99"
-  }];
-  chain = [{
-    id: 1,
-    timestamp: Date.now(),
-    user: {
-      name: "Akhilesh",
-      email: "nsakhilesh02@gmail.com",
-      mobile: "9611129344"
-    },
-    approver: {
-      name: "Aniruddha MN",
-      email: "aniruddha.murthy1@gmail.com"
-    },
-    url: "https://firebasestorage.googleapis.com/v0/b/shoppinglistplusplus-nsa.appspot.com/o/govdoc%2FLab%20record%20(4).pdf?alt=media&token=7d32e991-dbe8-4dd0-9e2b-61ee659dae99"
-  }];
+  pending = [];
+  chain = [];
 
   setRoute = route => this.currRoute = route;
   setUser = user => this.user = clone(user);
